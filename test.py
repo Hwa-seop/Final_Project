39d54f4cac3dc25f931639a81c1adbec62c2d783
wooseok-L2 <dntjr9009@knou.ac.kr>
2025-06-30 17:07:43 +0900

    Delete test.py
    delete

--- test.py
+++
@@ -1,7 +0,0 @@
-import cv2
-import numpy as np
-
-cv2.namedWindow("Test Window")
-cv2.imshow("Test Window", cv2.imread("test.jpg") or 255*np.ones((200, 200, 3), dtype=np.uint8))
-cv2.waitKey(0)
-cv2.destroyAllWindows()
